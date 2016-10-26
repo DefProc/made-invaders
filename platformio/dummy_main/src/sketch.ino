92b35bccbda721c291e616b23172b3a03b5b5046
Patrick Fenner <patrick@defproc.co.uk>
2016-10-26 10:45:58 +0100

    lots more comments

--- platformio/dummy_main/src/sketch.ino
+++ platformio/dummy_main/src/sketch.ino
@@ -3,84 +3,86 @@
 #include "constants.h"
 #include "secrets.h"
 
+// create the radio object
 RFM69 radio;
 
 // set the appropiate node ID
 #define NODEID MAIN_CTRL    // The Gateway runs on node 200
 
-// button and led connections
-#define START_BUTTON  13
-#define CANCEL_BUTTON 12
-
 // Lets get the global variables some space
-Payload theData; // somewhere to store incoming data
-Payload myPacket; // and one for oddutgoing
-game_states_t game_state = IDLE;
-long
-  current_score = 0;
-unsigned long
-  number_of_hits = 0,
-  start_time = 0,
-  game_uid = 0;
-volatile bool
-  update_scoreboard = false;
+Payload theData; // somewhere to store any incoming data
+Payload myPacket; // and one for outgoing radio data
+game_states_t game_state = IDLE; // hold the state for game operation
+long current_score = 0; // variable to hold the current score
+unsigned long number_of_hits = 0; // we also hold the total number of target hits
+unsigned long start_time = 0; // the millisecond register of the game start time
+unsigned long game_uid = 0; // unique game identifier
+volatile bool update_scoreboard = false; // do we need to update the scoreboard
 
-// set the target scores:
+// hold an array of the target scores:
 long scoremap[] = { 101, 236, 553, 100, 1234, 5120, -100, 999, 1024, 512, 256, 128, 200, 865, 6547, 450 };
+// and an array the same size for the number of hits per target
 unsigned long hit_record[sizeof(scoremap)];
 
 void setup() {
   // idiot pause on startup
   delay(1000);
 
+  // start the serial connection and print the start program message
   Serial.begin(BAUD);
   Serial.println("starting made-invaders dummy_main…");
 
   // initialize the radio
+  Serial.print(F("Radio…"));
   if (!radio.initialize(FREQUENCY,NODEID,NETWORKID)) {
-    // the radio doesn't exist
-    Serial.println(F("no radio"));
+    // the radio module isn't there!
+    Serial.println(F("FAIL"));
     while(1);
-  } else {
-    Serial.println(F("radio found"));
   }
+  Serial.println(F("OK"));
+
 #ifdef IS_RFM69HW
-  Serial.println(F("using high power radio mode"));
+  Serial.println(F("Radio HIGH POWER"));
   radio.setHighPower();
 #endif
+
+  // set the encryption key for the radio coms (defined in the secrets.h file)
   radio.encrypt(ENCRYPTKEY);
 
-  Serial.println(F("end of setup"));
+  Serial.println(F("setup END"));
 }
 
 uint16_t ackCount = 0;
 void loop() {
   // start the state machine
   if (game_state == IDLE) {
-    Serial.println(F("IDLE state"));
+    Serial.println(F("GAME_RESET"));
     // copy all the game variables into the right places for sending
     current_score = 0;
     myPacket.game_uid = game_uid;
     myPacket.impact_num = 0;
     myPacket.score = current_score;
     myPacket.timestamp = 30000;
-    // let everybody know what's happening
-    Serial.println(F("send scoreboardDisplay(current_score)"));
+    // update the scoreboard with the new zero score
+    Serial.println(F("display zero score"));
     scoreDisplay(current_score);
+    // then send a broadcast message (to every receiver) to get ready
     Serial.println(F("broadcast GET_READY"));
     broadcastMessage(GET_READY);
-    // lets at least try to make sure the registration station gets this
-    //radio.sendWithRetry(REG_STN, (const void*)&myPacket, sizeof(myPacket), 5);
+
     Serial.println(F("Ready to play (press \'S\')"));
+    // move out of this game state, so we don't do this again
     game_state = RFID_SCANNED;
   } else if (game_state == RUNNING) {
     // lets' keep this fast, so score updates happen quickly
     checkTargets();
 
     if (update_scoreboard == true) {
-      // there's been a new hit, send that
+      // there's been a new hit, send that to the scoreboard
       scoreDisplay(current_score);
+      Serial.print(F("SCORE: "));
       Serial.println(current_score);
+      // and then don't send it again
       update_scoreboard = false;
     }
 
@@ -103,20 +105,24 @@ void loop() {
     // increment the counters for the next game
     game_uid++;
 
+    // change state so we don't do this again
     game_state = RFID_SCANNED;
-    Serial.println(F("Game stopped (press \'I\' to go to idle)"));
+    Serial.println(F("Game stopped (press \'R\' to reset)"));
    }
 
+  // every time through the loop, we'll check for serial instructions
   if (Serial.available() > 0) {
+    // read the incoming character (char)
     char sue = Serial.read();
+    // check if it matches any of our accepted instructions (H,R,S,X)
     if (sue == 'h' || sue == 'H') {
       // print out the help message
       Serial.println(F("made-invaders dummy_main controller"));
-      Serial.println(F("I - Idle mode (prep for next game)"));
+      Serial.println(F("R - Reset"));
       Serial.println(F("S - Start game"));
-      Serial.println(F("X - Stop game (and go to idle)"));
-    } else if (sue == 'i' || sue == 'I') {
-      // idle mode
+      Serial.println(F("X - Stop game"));
+    } else if (sue == 'r' || sue == 'R') {
+      // reset the game
       game_state = IDLE;
     } else if (sue == 's' || sue == 'S') {
       // start game
@@ -128,32 +134,27 @@ void loop() {
       game_state = END_GAME;
     }
     while (Serial.available() > 0) {
-      // flush the serial buffer
+      // flush any remaining character in the serial buffer
+      // as we only care about single character messages
       Serial.read();
     }
   }
 }
 
-void Blink(byte PIN, int DELAY_MS)
-{
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN,HIGH);
-  delay(DELAY_MS);
-  digitalWrite(PIN,LOW);
-}
-
+// a helper function to make it easier to send numbers to the scoreboard
 void scoreDisplay(int32_t a_number) {
   Payload aPacket;
   aPacket.message_id = DISPLAY_NUM;
   aPacket.score = a_number;
   if (radio.sendWithRetry(SCOREBD, (const void*)&aPacket, sizeof(aPacket), 2, 50)) {
-    Serial.print(F("display score: "));
+    Serial.print(F("Display: "));
     Serial.println(myPacket.score);
   } else {
-    Serial.println(F("No ACK from scoreboard"));
+    Serial.println(F("No response (ACK) from scoreboard"));
   }
 }
 
+// a helper function to send broadcast messages
 void broadcastMessage(message_t the_message) {
   myPacket.message_id = the_message;
   myPacket.timestamp = millis() - start_time;
@@ -162,42 +163,45 @@ void broadcastMessage(message_t the_message) {
   Serial.println(the_message);
 }
 
+// check incoming messages while the game is running
 void checkTargets() {
   // check any incoming radio messages
-  // *** We need to call this fast enough in-game ***
+  // *** We need to call this fast enough in-game that we don't miss any***
   if (radio.receiveDone()) {
     // check if we're getting hit data
     if (radio.ACKRequested() && radio.SENDERID <= NUM_TARGETS) {
-      //uint8_t theNodeID = radio.SENDERID;
+      // send an ACK (acknowledgement) straight away if requested
       radio.sendACK();
-      Serial.print(F("ACK "));
+      Serial.print(F("ACK:"));
       Serial.println(radio.SENDERID);
     }
     if (radio.DATALEN = sizeof(Payload)) {
       theData = *(Payload*)radio.DATA;
       if (theData.message_id == HIT) {
+        // the target radio ids start at 1
+        // but the target arrays are zero indexed
         uint8_t this_target = radio.SENDERID-1;
+        // print the message to serial so we can see
         Serial.print(F("HIT: "));
         Serial.print(this_target);
         Serial.print(F(" SCORED: "));
         Serial.println(scoremap[this_target]);
-        if (this_target >= NUM_TARGETS) { this_target = NUM_TARGETS-1; }
-        long this_score = scoremap[this_target];
-        if (game_state == COUNTDOWN) {
-          current_score = current_score + (this_score*2);
-        } else {
-          current_score = current_score + (this_score);
+
+        if (this_target <= NUM_TARGETS) {
+          // add the target score to the current score
+          current_score = current_score + (scoremap[this_target]);
+          // set the hit counter for the target to the incoming value
+          hit_record[this_target] = theData.impact_num;
+          // set the flag so the scoreboard will be updated
+          update_scoreboard = true;
         }
-        hit_record[this_target] = theData.impact_num;
-        update_scoreboard = true;
       }
     }
   }
 }
 
 void radioAcks() {
-  // check any incoming radio messages
-  // *** We need to call this fast enough in-game ***
+  // automatically ACK any messages
   if (radio.receiveDone()) {
     // check if we're getting hit data
     radio.sendACK();
